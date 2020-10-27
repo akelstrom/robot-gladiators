4a29077eb8e041f811cad426f44c9348ae9c923f
akelstrom <akelstrom@gmail.com>
2020-10-27 15:01:58 -0400

    experimenting with variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+//Note there are no quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message.");
+//this will do math and log 20
+console.log(10 + 10);
+console.log("our robot's name is " + playerName)
+console.log(playerName + " is ready for battle!");
+console.log("Your robot, " + playerName + ", has won!")
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
